73d24a026e413b6e40cad9d31cc9a5a6c38b2e3d
Gabriel Wisky Benatti <gabrielwiskybenatti@gmail.com>
2022-12-21 21:42:43 -0300

    criado widget de titulo de sessão

---
+++ app_kp_merch/lib/src/widgets/kp_section_title.dart
@@ -0,0 +1,29 @@
+import 'package:app_kp_merch/src/utils/kp_theme.dart';
+import 'package:flutter/material.dart';
+
+class KpSectionTitle extends StatelessWidget {
+  const KpSectionTitle(
+    this.text, {
+    super.key,
+  });
+
+  final String text;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(
+        left: KpTheme.kDefaultPadding,
+        top: KpTheme.kDefaultPadding * 2,
+        right: KpTheme.kDefaultPadding,
+        bottom: KpTheme.kDefaultPadding,
+      ),
+      child: Text(
+        text,
+        style: const TextStyle(
+          fontSize: 22.0,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+}
