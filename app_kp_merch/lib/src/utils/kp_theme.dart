677d9144e4ef063597e1dd516292495f7d4b1dcf
Gabriel Wisky Benatti <gabrielwiskybenatti@gmail.com>
2022-12-21 21:42:22 -0300

    criado classe com infos do tema

---
+++ app_kp_merch/lib/src/utils/kp_theme.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart' show Color, Colors;
+
+class KpTheme {
+  static double kDefaultPadding = 16.0;
+  static double kDefaultRadius = 16.0;
+
+  static Color kPrimaryColor = const Color(0xFFB52600);
+
+  static Color kPrimaryWhite = const Color(0xFFFFFFFF);
+  static Color kSecondaryWhite = const Color(0xFFF9F9F9);
+  static Color kPrimaryBlack = Colors.black87;
+}
