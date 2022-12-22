1e7302809d214927abe7a6a1a32e3e85a9d7cc68
Gabriel Wisky Benatti <gabrielwiskybenatti@gmail.com>
2022-12-21 21:37:26 -0300

    Create home_screen.dart

---
+++ app_kp_merch/lib/src/modules/home/home_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class KpHomeScreen extends StatefulWidget {
+  const KpHomeScreen({super.key});
+
+  @override
+  State<KpHomeScreen> createState() => _KpHomeScreenState();
+}
+
+class _KpHomeScreenState extends State<KpHomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
