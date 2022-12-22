2b663c6c5a67ce399fee915f1095091059557453
Gabriel Wisky Benatti <gabrielwiskybenatti@gmail.com>
2022-12-21 22:02:47 -0300

    adicionado área do carrossel e appbar

--- app_kp_merch/lib/src/modules/home/home_screen.dart
+++ app_kp_merch/lib/src/modules/home/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:app_kp_merch/src/utils/kp_theme.dart';
+
 class KpHomeScreen extends StatefulWidget {
   const KpHomeScreen({super.key});
 
@@ -10,6 +12,34 @@ class KpHomeScreen extends StatefulWidget {
 class _KpHomeScreenState extends State<KpHomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: KpTheme.kPrimaryWhite,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.shopping_cart_outlined,
+              color: KpTheme.kPrimaryBlack,
+            ),
+          ),
+        ],
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            margin: EdgeInsets.only(
+              top: KpTheme.kDefaultPadding,
+            ),
+            color: Colors.blue,
+            height: 190,
+            child: const Center(
+              child: Text('Carrossel'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
