7eee3a0aa9d34fe606f6e4a57c6221bd842a2b55
Gabriel Wisky Benatti <gabrielwiskybenatti@gmail.com>
2022-12-21 21:45:05 -0300

    criado widget de exibição de produto

---
+++ app_kp_merch/lib/src/widgets/kp_product_horizontal.dart
@@ -0,0 +1,60 @@
+// import 'package:app_kp_merch/app/models/product_model.dart';
+import 'package:app_kp_merch/src/utils/kp_theme.dart';
+import 'package:flutter/material.dart';
+
+class KpProductHorizontal extends StatelessWidget {
+  const KpProductHorizontal(
+    this.name,
+    this.amount, {
+    super.key,
+  });
+
+  final String name;
+
+  final double amount;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: KpTheme.kPrimaryWhite,
+      padding: EdgeInsets.symmetric(
+        horizontal: KpTheme.kDefaultPadding,
+        vertical: KpTheme.kDefaultPadding / 4,
+      ),
+      child: Row(
+        children: [
+          Container(
+            margin: EdgeInsets.only(right: KpTheme.kDefaultPadding / 2),
+            decoration: BoxDecoration(
+              color: KpTheme.kPrimaryColor,
+              borderRadius: BorderRadius.circular(
+                KpTheme.kDefaultPadding,
+              ),
+            ),
+            width: 60,
+            height: 60,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: const [
+              Text(
+                'name',
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+                style: /*const*/ TextStyle(
+                  fontSize: 18.0,
+                ),
+              ),
+              Text(
+                'R\$ 999',
+                style: /*const*/ TextStyle(
+                  fontSize: 16.0,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
