e45391033dd3b93c345a17683d08ba22e442949d
Gabriel Wisky Benatti <gabrielwiskybenatti@gmail.com>
2022-12-27 13:02:45 -0300

    Create product_detail.dart

---
+++ app_kp_merch/lib/app/modules/products/product_detail.dart
@@ -0,0 +1,16 @@
+import 'package:app_kp_merch/app/data/models/product_model.dart';
+import 'package:flutter/material.dart';
+
+class ProductView extends StatelessWidget {
+  const ProductView(
+    this.product, {
+    super.key,
+  });
+
+  final ProductModel product;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
