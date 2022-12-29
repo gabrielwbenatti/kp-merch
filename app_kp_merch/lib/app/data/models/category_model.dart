a31eca4317d0dce038247eb0c18aa72aeef3df20
Gabriel Wisky Benatti <gabrielwiskybenatti@gmail.com>
2022-12-29 11:18:25 -0300

    propriedade name obrigatória

--- app_kp_merch/lib/app/data/models/category_model.dart
+++ app_kp_merch/lib/app/data/models/category_model.dart
@@ -1,10 +1,11 @@
 import 'package:app_kp_merch/app/data/models/base_model.dart';
 
 class CategoryModel extends BaseModel {
-  CategoryModel({
-    this.name,
+  CategoryModel(
+    this.name, {
+    this.id,
   });
 
   String? id;
-  String? name;
+  String name;
 }
