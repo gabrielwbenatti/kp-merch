895856b52219332d54fb06f38e45b4aaa3d525e7
Gabriel Wisky Benatti <gabrielwiskybenatti@gmail.com>
2022-12-29 11:17:55 -0300

    mudanças no widget de categoria

--- app_kp_merch/lib/app/widgets/kp_category_circle.dart
+++ app_kp_merch/lib/app/widgets/kp_category_circle.dart
@@ -1,113 +1,68 @@
 import 'package:flutter/material.dart';
 
+import 'package:app_kp_merch/app/data/models/category_model.dart';
 import 'package:app_kp_merch/app/utils/kp_theme.dart';
 
 class KpCategoryCircle extends StatelessWidget {
   const KpCategoryCircle(
-    this.text,
+    this.category,
     this.icon, {
     this.isLast = false,
     this.isFirst = false,
     super.key,
   });
 
-  final String text;
   final IconData icon;
   final bool isLast;
   final bool isFirst;
+  final CategoryModel category;
 
   @override
   Widget build(BuildContext context) {
-    return _op2();
-  }
-
-  Widget _op2() {
-    return Container(
-      width: KpTheme.kDefaultPadding * 6,
-      margin: isLast
+    return Padding(
+      padding: isLast
           ? EdgeInsets.symmetric(horizontal: KpTheme.kDefaultPadding)
           : EdgeInsets.only(left: KpTheme.kDefaultPadding),
-      padding: EdgeInsets.fromLTRB(
-        KpTheme.kDefaultPadding / 2,
-        KpTheme.kDefaultPadding,
-        KpTheme.kDefaultPadding / 2,
-        KpTheme.kDefaultPadding,
-      ),
-      decoration: BoxDecoration(
+      child: Material(
         color: KpTheme.kPrimaryColor,
         borderRadius: BorderRadius.circular(KpTheme.kDefaultRadius),
-      ),
-      child: Column(
-        children: [
-          Container(
-            decoration: BoxDecoration(
-              color: KpTheme.kPrimaryColor,
-              shape: BoxShape.circle,
-            ),
-            margin: EdgeInsets.only(bottom: KpTheme.kDefaultPadding),
-            child: Icon(
-              icon,
-              size: 26.0,
-              color: KpTheme.kPrimaryWhite,
-            ),
-          ),
-          Text(
-            text,
-            overflow: TextOverflow.ellipsis,
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              color: KpTheme.kPrimaryWhite,
-              // fontSize: 16.0,
-            ),
-          )
-        ],
-      ),
-    );
-  }
-
-  Widget _op1() {
-    return Container(
-      width: KpTheme.kDefaultPadding * 6,
-      padding: isFirst
-          ? EdgeInsets.only(
-              left: KpTheme.kDefaultPadding,
+        child: InkWell(
+          onTap: () {},
+          borderRadius: BorderRadius.circular(KpTheme.kDefaultRadius),
+          child: Container(
+            padding: EdgeInsets.only(
+              left: KpTheme.kDefaultPadding / 2,
+              top: KpTheme.kDefaultPadding,
               right: KpTheme.kDefaultPadding / 2,
-            )
-          : isLast
-              ? EdgeInsets.only(
-                  right: KpTheme.kDefaultPadding,
-                )
-              : EdgeInsets.symmetric(
-                  horizontal: KpTheme.kDefaultPadding / 2,
-                ),
-      child: Column(
-        children: [
-          Container(
-            margin: EdgeInsets.only(
-              bottom: KpTheme.kDefaultPadding / 2,
+              bottom: KpTheme.kDefaultPadding,
             ),
-            padding: EdgeInsets.all(
-              KpTheme.kDefaultPadding * 1.2,
-            ),
+            width: KpTheme.kDefaultPadding * 6,
             decoration: BoxDecoration(
-              color: KpTheme.kPrimaryColor,
-              shape: BoxShape.circle,
+              borderRadius: BorderRadius.circular(KpTheme.kDefaultRadius),
             ),
-            child: Icon(
-              icon,
-              size: 26.0,
-              color: KpTheme.kPrimaryWhite,
+            child: Column(
+              children: [
+                Container(
+                  margin: EdgeInsets.only(bottom: KpTheme.kDefaultPadding),
+                  child: Icon(
+                    icon,
+                    size: 26.0,
+                    color: KpTheme.kPrimaryWhite,
+                  ),
+                ),
+                Text(
+                  category.name,
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: KpTheme.kPrimaryWhite,
+                    // fontSize: 16.0,
+                  ),
+                )
+              ],
             ),
           ),
-          Text(
-            text,
-            overflow: TextOverflow.ellipsis,
-            style: const TextStyle(
-              fontWeight: FontWeight.bold,
-              // fontSize: 16.0,
-            ),
-          )
-        ],
+        ),
       ),
     );
   }
