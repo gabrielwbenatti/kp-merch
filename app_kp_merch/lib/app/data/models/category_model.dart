e2ee1c4af41cd467a373321e19702228ccf7e754
Gabriel Wisky Benatti <gabrielwiskybenatti@gmail.com>
2023-01-17 20:50:29 -0300

    add serialization json methods

--- app_kp_merch/lib/app/data/models/category_model.dart
+++ app_kp_merch/lib/app/data/models/category_model.dart
@@ -1,11 +1,32 @@
+import 'dart:convert';
+
 import 'package:app_kp_merch/app/data/models/base_model.dart';
 
 class CategoryModel extends BaseModel {
-  CategoryModel(
-    this.name, {
+  CategoryModel({
     this.id,
+    required this.name,
   });
 
   String? id;
   String name;
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': id,
+      'name': name,
+    };
+  }
+
+  factory CategoryModel.fromMap(Map<String, dynamic> map) {
+    return CategoryModel(
+      id: map['id'],
+      name: map['name'] ?? 'UNDEFINED',
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory CategoryModel.fromJson(String source) =>
+      CategoryModel.fromMap(json.decode(source));
 }
