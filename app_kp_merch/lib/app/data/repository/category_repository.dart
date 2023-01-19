24809826107110f8001f37f313e65d111ee46a4f
Gabriel Wisky Benatti <gabrielwiskybenatti@gmail.com>
2023-01-19 11:44:35 -0300

    add fetch produtos por categoria

--- app_kp_merch/lib/app/data/repository/category_repository.dart
+++ app_kp_merch/lib/app/data/repository/category_repository.dart
@@ -1,6 +1,8 @@
+import 'package:dio/dio.dart';
+
 import 'package:app_kp_merch/app/data/api.dart';
 import 'package:app_kp_merch/app/data/models/category_model.dart';
-import 'package:dio/dio.dart';
+import 'package:app_kp_merch/app/data/models/product_model.dart';
 
 class CategoryRepository {
   final dio = Dio();
@@ -11,6 +13,18 @@ class CategoryRepository {
           await dio.get('${KpMerchApi.BASE_URL}/products/categories');
       final list = response.data as List;
       return list.map((category) => CategoryModel.fromMap(category)).toList();
+    } catch (e) {
+      throw Exception(e);
+    }
+  }
+
+  Future<List<ProductModel>> fetchProductsPerCategory(
+      String categoryName) async {
+    try {
+      final response = await dio
+          .get('${KpMerchApi.BASE_URL}/products/category/$categoryName');
+      final list = response.data as List;
+      return list.map((product) => ProductModel.fromMap(product)).toList();
     } catch (e) {
       throw Exception(e);
     }
