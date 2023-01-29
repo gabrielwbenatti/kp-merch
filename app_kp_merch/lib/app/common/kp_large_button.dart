3eec5eda9c0b1157115a64cd85035e8db59e9737
Gabriel Wisky Benatti <gabrielwiskybenatti@gmail.com>
2023-01-29 10:52:53 -0300

    adicionado card de total

--- app_kp_merch/lib/app/common/kp_large_button.dart
+++ app_kp_merch/lib/app/common/kp_large_button.dart
@@ -4,38 +4,27 @@ import 'package:flutter/material.dart';
 class KpLargeButton extends StatelessWidget {
   const KpLargeButton(
     this.label, {
-    this.icon,
+    required this.icon,
     this.onPressed,
     super.key,
   });
 
   final String label;
-  final IconData? icon;
+  final IconData icon;
   final VoidCallback? onPressed;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.all(KpTheme.kDefaultPadding),
+      padding: EdgeInsets.all(KpTheme.kDefaultPadding / 2),
       child: SizedBox(
         width: double.infinity,
-        child: _buildChild(),
+        child: FilledButton.tonalIcon(
+          onPressed: onPressed,
+          icon: Icon(icon),
+          label: Text(label),
+        ),
       ),
     );
-  }
-
-  _buildChild() {
-    if (icon != null) {
-      return FilledButton.tonalIcon(
-        onPressed: onPressed,
-        icon: Icon(icon),
-        label: Text(label),
-      );
-    } else {
-      FilledButton.tonal(
-        onPressed: onPressed,
-        child: Text(label),
-      );
-    }
   }
 }
--- app_kp_merch/lib/app/modules/cart/kp_cart_screen.dart
+++ app_kp_merch/lib/app/modules/cart/kp_cart_screen.dart
@@ -1,15 +1,92 @@
+import 'dart:ffi';
+
+import 'package:app_kp_merch/app/common/kp_large_button.dart';
 import 'package:app_kp_merch/app/data/models/cart_item_model.dart';
+import 'package:app_kp_merch/app/utils/kp_theme.dart';
 import 'package:flutter/material.dart';
 
 import 'package:app_kp_merch/app/data/models/product_model.dart';
 import 'package:app_kp_merch/app/modules/cart/widgets/kp_cart_item.dart';
 
-class KpCartScreen extends StatelessWidget {
+class KpCartScreen extends StatefulWidget {
   const KpCartScreen({super.key});
 
   @override
+  State<KpCartScreen> createState() => _KpCartScreenState();
+}
+
+class _KpCartScreenState extends State<KpCartScreen> {
+  @override
   Widget build(BuildContext context) {
     int itemCount = 12;
+    var panelKey = GlobalKey();
+
+    Widget _buildTotalPanel() {
+      return Positioned(
+        bottom: 0,
+        left: 0,
+        right: 0,
+        child: Container(
+          key: panelKey,
+          padding: EdgeInsets.all(KpTheme.kDefaultPadding),
+          margin: const EdgeInsets.all(5),
+          width: double.infinity,
+          decoration: BoxDecoration(
+            color: Theme.of(context).colorScheme.background,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: const [
+                  Text("Total do pedido"),
+                  Text("R\$56165,00"),
+                ],
+              ),
+              KpLargeButton(
+                'Continuar',
+                icon: Icons.shopping_cart_checkout_outlined,
+                onPressed: () {},
+              )
+            ],
+          ),
+        ),
+      );
+    }
+
+    Widget _buildItemsList() {
+      return SingleChildScrollView(
+        child: Column(
+          children: [
+            ListView.builder(
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              itemCount: itemCount,
+              itemBuilder: (context, index) {
+                final product = ProductModel(
+                  name: 'name $index',
+                  price: 365.0,
+                );
+
+                final cartItem = CartItemModel(
+                  product: product,
+                  size: 30,
+                );
+
+                return KpCartItem(
+                  cartItem,
+                  isLast: index == (itemCount - 1),
+                );
+              },
+            ),
+            const SizedBox(
+              height: 120,
+            ),
+          ],
+        ),
+      );
+    }
 
     return Scaffold(
       appBar: AppBar(
@@ -17,24 +94,11 @@ class KpCartScreen extends StatelessWidget {
           'Meu Carrinho',
         ),
       ),
-      body: ListView.builder(
-        itemCount: itemCount,
-        itemBuilder: (context, index) {
-          final product = ProductModel(
-            name: 'name',
-            price: 365.0,
-          );
-
-          final cartItem = CartItemModel(
-            product: product,
-            size: 30,
-          );
-
-          return KpCartItem(
-            cartItem,
-            isLast: index == (itemCount - 1),
-          );
-        },
+      body: Stack(
+        children: [
+          _buildItemsList(),
+          _buildTotalPanel(),
+        ],
       ),
     );
   }
